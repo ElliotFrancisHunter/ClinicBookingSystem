486bf5010ca40ac065c40b1f237dfba8cb5aee67
Elliot Francis Hunter <elliot.hunteruk@yahoo.co.uk>
2019-07-12 08:05:22 +0100

    Code review and refactoring.
    Clean up of code and improve documentation. Removed redundant code or declarations. Improved formatting and general organisation, quality of life stuff.

--- SampleService/ClinicBookingApplication/OverviewScreen.aspx.cs
+++ SampleService/ClinicBookingApplication/OverviewScreen.aspx.cs
@@ -1,9 +1,16 @@
-﻿
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="OverviewScreen.aspx.cs" company="ServelecHsc">
+//   Elliot Hunter 
+// </copyright>
+// <summary>
+//   The User interface.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
 namespace ClinicBookingApplication
 {
     using System;
     using System.Data;
-    using System.Web.Services.Description;
     using System.Web.UI;
     using ClinicBookingApplication.ClinicBookingService;
 
@@ -13,41 +20,14 @@ namespace ClinicBookingApplication
     public partial class WebForm1 : Page
     {
         /// <summary>
-        /// 
+        /// A function that creates a data table for all patients. This is displayed in a drop down.
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            if (this.IsPostBack)
-            {
-                return;
-            }
-
-            this.MakePatientTable();
-            this.MakeDurationTable();
-            this.MakeClinicTable();
-            this.MakeUrgencyTable();
-            this.MakeAppointmentTypeTable();
-            this.MakeCurrentAppointmentsTable();
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
         public void MakePatientTable()
         {
             var dataTable = new DataTable();
             dataTable.Columns.Add("PatientNo");
             dataTable.Columns.Add("Patient Name");
-            var patientList = new ClinicBookingService.SampleServiceClient().GetPatients();
+            var patientList = new SampleServiceClient().GetPatients();
 
             foreach (var patient in patientList)
             {
@@ -60,12 +40,15 @@ namespace ClinicBookingApplication
             this.PatientDropDownList.DataBind();
         }
 
+        /// <summary>
+        /// Creates data table for all appointment durations stored on database.
+        /// </summary>
         public void MakeDurationTable()
         {
             var dataTable = new DataTable();
             dataTable.Columns.Add("DurationID");
             dataTable.Columns.Add("AppointmentLength");
-            var durationList = new ClinicBookingService.SampleServiceClient().GetAllAppointmentDurationData();
+            var durationList = new SampleServiceClient().GetAllAppointmentDurationData();
 
             foreach (var duration in durationList)
             {
@@ -76,15 +59,17 @@ namespace ClinicBookingApplication
             this.DurationDropDownList.DataTextField = "AppointmentLength";
             this.DurationDropDownList.DataValueField = "DurationID";
             this.DurationDropDownList.DataBind();
-
         }
 
+        /// <summary>
+        /// Creates data table for all clinics stored on database.
+        /// </summary>
         public void MakeClinicTable()
         {
             var dataTable = new DataTable();
             dataTable.Columns.Add("ClinicID");
             dataTable.Columns.Add("CodeDescription");
-            var clinicList = new ClinicBookingService.SampleServiceClient().GetClinics();
+            var clinicList = new SampleServiceClient().GetClinics();
 
             foreach (var clinic in clinicList)
             {
@@ -97,12 +82,15 @@ namespace ClinicBookingApplication
             this.ClinicDropDownList.DataBind();
         }
 
+        /// <summary>
+        /// Creates data table for all appointment urgencies stored on database.
+        /// </summary>
         public void MakeUrgencyTable()
         {
             var dataTable = new DataTable();
             dataTable.Columns.Add("UrgencyID");
             dataTable.Columns.Add("UrgencyDescriptor");
-            var urgencyList = new ClinicBookingService.SampleServiceClient().GetUrgencies();
+            var urgencyList = new SampleServiceClient().GetUrgencies();
 
             foreach (var urgency in urgencyList)
             {
@@ -115,12 +103,15 @@ namespace ClinicBookingApplication
             this.UrgencyDropDownList.DataBind();
         }
 
+        /// <summary>
+        /// Creates data table for all appointment types stored on database.
+        /// </summary>
         public void MakeAppointmentTypeTable()
         {
             var dataTable = new DataTable();
             dataTable.Columns.Add("AppointmentTypeID");
             dataTable.Columns.Add("TypeDescriptor");
-            var appointmentTypeList = new ClinicBookingService.SampleServiceClient().GetAppointmentTypes();
+            var appointmentTypeList = new SampleServiceClient().GetAppointmentTypes();
 
             foreach (var type in appointmentTypeList)
             {
@@ -133,18 +124,21 @@ namespace ClinicBookingApplication
             this.AppointmentTypeDropDownList.DataBind();
         }
 
+        /// <summary>
+        /// Function that builds the data table that stores all appointments stored in database ready for display on frontend.
+        /// </summary>
         public void MakeCurrentAppointmentsTable()
         {
             var dataTable = new DataTable();
-            var appointments = new ClinicBookingService.SampleServiceClient().GetAppointments();
-            dataTable.Columns.AddRange(new DataColumn[6]
+            var appointments = new SampleServiceClient().GetAppointments();
+            dataTable.Columns.AddRange(new[]
                                            {
                                                new DataColumn("AppointmentID", typeof(string)),
                                                new DataColumn("IsActive", typeof(bool)),
                                                new DataColumn("PatientID", typeof(string)),
                                                new DataColumn("StartDateTime", typeof(string)),
                                                new DataColumn("DurationID", typeof(string)),
-                                               new DataColumn("ClinicID", typeof(string)),
+                                               new DataColumn("ClinicID", typeof(string))
                                            });
              
             foreach (var appointment in appointments)
@@ -168,6 +162,39 @@ namespace ClinicBookingApplication
             this.SearchResultsGrid.DataBind();
         }
 
+        /// <summary>
+        /// All functions handled on loading the page.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender object that performs actions.
+        /// </param>
+        /// <param name="e">
+        /// The event that acts as a trigger. In this case loading a page.
+        /// </param>
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (this.IsPostBack)
+            {
+                return;
+            }
+
+            this.MakePatientTable();
+            this.MakeDurationTable();
+            this.MakeClinicTable();
+            this.MakeUrgencyTable();
+            this.MakeAppointmentTypeTable();
+            this.MakeCurrentAppointmentsTable();
+        }
+
+        /// <summary>
+        /// The function that handles all actions taken when button is clicked, in this case creating a new appointment and adding it to the database.
+        /// </summary>
+        /// <param name="sender">
+        /// The object that performs required actions.
+        /// </param>
+        /// <param name="e">
+        /// The trigger. Clicking the button.
+        /// </param>
         protected void NewAppointmentButtonClick(object sender, EventArgs e)
         {                                        
             new SampleServiceClient().SetAppointment(
@@ -182,24 +209,56 @@ namespace ClinicBookingApplication
             this.MakeCurrentAppointmentsTable();
         }
 
-        protected void SearchResultsGrid_SelectedIndexChanged(object sender, EventArgs e)
+        /// <summary>
+        /// Functions carried out once value in drop down is changed.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender object that performs required actions.
+        /// </param>
+        /// <param name="e">
+        /// The events that acts as a trigger.
+        /// </param>
+        protected void DropDownList1SelectedIndexChanged(object sender, EventArgs e)
         {
-
         }
 
+        /// <summary>
+        /// Function that handles any actions taken when index of grid changed.
+        /// </summary>
+        /// <param name="sender">
+        /// The object that initiates actions.
+        /// </param>
+        /// <param name="e">
+        /// The event trigger.
+        /// </param>
+        protected void SearchResultsGridSelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Function that handles any actions taken when contents of text box is changed.
+        /// </summary>
+        /// <param name="sender">
+        /// The object that initiates actions.
+        /// </param>
+        /// <param name="e">
+        /// The event trigger.
+        /// </param>
         protected void DateTimeTextBoxTextChanged(object sender, EventArgs e)
         {
-            var inputDateTime = Convert.ToDateTime(this.DateTimeTextBox.Text);
         }
 
-        protected void DateTimeTextBox_TextChanged(object sender, EventArgs e)
+        /// <summary>
+        /// Function that handles any actions taken when contents of search box is changed.
+        /// </summary>
+        /// <param name="sender">
+        /// The object that initiates actions.
+        /// </param>
+        /// <param name="e">
+        /// The event trigger.
+        /// </param>
+        protected void SearchByTagTextBoxTextChanged(object sender, EventArgs e)
         {
-
-        }
-
-        protected void SearchByTagTextBox_TextChanged(object sender, EventArgs e)
-        {
-
         }
     }
 }
