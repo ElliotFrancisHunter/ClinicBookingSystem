98c635a2a5dac1976233255ac51d387fa78cdf9c
Elliot Francis Hunter <elliot.hunteruk@yahoo.co.uk>
2019-07-12 07:04:00 +0100

    Formatting

--- SampleService/ClinicBookingApplication/OverviewScreen.aspx.cs
+++ SampleService/ClinicBookingApplication/OverviewScreen.aspx.cs
@@ -178,6 +178,7 @@ namespace ClinicBookingApplication
                 this.ClinicDropDownList.SelectedValue,
                 this.UrgencyDropDownList.SelectedValue,
                 this.AppointmentTypeDropDownList.SelectedValue);
+
             this.MakeCurrentAppointmentsTable();
         }
 
